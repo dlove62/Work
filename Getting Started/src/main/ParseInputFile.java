dcf86c9853fa9c655886697ff32b72c2fd64fac6
David <dlove62@sbcgloblal.net>
2015-08-09 13:28:11 -0500

    added OutLine Class

--- Getting Started/src/main/ParseInputFile.java
+++ Getting Started/src/main/ParseInputFile.java
@@ -10,119 +10,70 @@ public class ParseInputFile {
 	public static void main(String[] args) {
 		File newFile = new File("C:/Bimedit/work/JCL_Out.txt");
 		BufferedReader textReader = null;
-		String jobName = null;
-		String jobProgName = null;
-		String jobClass = null;
-		String jobMsgClass = null;
-		String jobMemberName = null;
-		String jobEnvironment = null;
-		String jobRegion = null;
-		String jobProgram = null;
+
 		String jobTempFileName = null;
 		String rptLength = null;
 		String ftpParmLibName = null;
 		String ftpPath = null;
 		String ftpRptFileName = null;
-		String genSysNumber = null;
-		String genPrintClass = null;
-		String genWriterName = null;
-		String genPrintDest = null;
-		String genFormId = null;
+
 		boolean somethingToPrint = false;
 		Line lineObj;
-		
+		OutLine myOutLine = new OutLine();
+
 		try {
-			// int numberOfLines = 0;
 			FileReader reader = new FileReader(newFile);
 			textReader = new BufferedReader(reader);
-			//int numberOfLines = countLines("C:/Bimedit/work/JCL_Out.txt");
 			BufferedReader br = null;
-			// String[] textData = new String[70];
-			// FileReader fr = new FileReader(path);
 			br = new BufferedReader(reader);
 			String aLine = null;
-			
+
 			while ((aLine = br.readLine()) != null) {
-				// System.out.println(aLine);
 				lineObj = new Line(aLine);
 				if (aLine.contains(" JOB ")) {
 					if (somethingToPrint) {
-						writeOutput(jobName, jobProgName, jobClass, jobMsgClass, jobMemberName, jobEnvironment,
-								jobRegion, jobProgram, genSysNumber, genPrintClass, genWriterName, genPrintDest,
-								genFormId);
+						myOutLine.printOutLine();
 						somethingToPrint = true;
 					}
-
-					jobName = "";
-					jobProgName = "";
-					jobClass = "";
-					jobMsgClass = "";
-					jobMemberName = "";
-					jobEnvironment = "";
-					jobRegion = "";
-					jobProgram = "";
-					genSysNumber = "";
-					genPrintClass = "";
-					genWriterName = "";
-					genPrintDest = "";
-					genFormId = "";
+					myOutLine.clearOutLine();
 					somethingToPrint = true;
-
 					lineObj = new Line(aLine);
-					jobName = lineObj.getToken(1);
-					jobProgName = lineObj.getToken(4);
-				}
-				else if (aLine.contains(" CLASS=")) {
-					jobClass = lineObj.getToken(2);
-				}
-				else if (aLine.contains("MSGCLASS=")) {
-					jobMsgClass = lineObj.getToken(2);
-				}
-				else if (aLine.contains(" SET JOBMBR")) {
-					jobMemberName = lineObj.getToken(4);
-				}
-				else if (aLine.contains(" SET ENV")) {
-					jobEnvironment = lineObj.getToken(4);
-				}
-				else if (aLine.contains(" SET  RG")) {
-					jobRegion = lineObj.getToken(4);
-				}
-				else if (aLine.contains(" EXEC ")) {
-					jobProgram = lineObj.getToken(4);
+					myOutLine.setJobName(lineObj.getToken(1));
+					myOutLine.setJobProgName(lineObj.getToken(4));
+				} else if (aLine.contains(" CLASS=")) {
+					myOutLine.setJobClass(lineObj.getToken(2));
+				} else if (aLine.contains("MSGCLASS=")) {
+					myOutLine.setJobMsgClass(lineObj.getToken(2));
+				} else if (aLine.contains(" SET JOBMBR")) {
+					myOutLine.setJobMemberName(lineObj.getToken(4));
+				} else if (aLine.contains(" SET ENV")) {
+					myOutLine.setJobEnvironment(lineObj.getToken(4));
+				} else if (aLine.contains(" SET  RG")) {
+					myOutLine.setJobRegion(lineObj.getToken(4));
+				} else if (aLine.contains(" EXEC ")) {
+					myOutLine.setJobProgram(lineObj.getToken(4));
 					somethingToPrint = true;
-				}
-				else if (aLine.contains(" SYSOUT=(")) {
-					genSysNumber = lineObj.getToken(1);
-					genPrintClass = lineObj.getToken(4);
-					genWriterName = lineObj.getToken(5);
-				}
-				else if (aLine.contains(" DUMMY")) {
-					genSysNumber = lineObj.getToken(1);
-					genPrintClass = lineObj.getToken(3);
-					writeOutput(jobName, jobProgName, jobClass, jobMsgClass, jobMemberName, jobEnvironment, jobRegion,
-							jobProgram, genSysNumber, genPrintClass, genWriterName, genPrintDest, genFormId);
+				} else if (aLine.contains(" SYSOUT=(")) {
+					myOutLine.setGenSysNumber(lineObj.getToken(1));
+					myOutLine.setGenPrintClass(lineObj.getToken(3));
+					myOutLine.setGenWriterName(lineObj.getToken(5));
+				} else if (aLine.contains(" DUMMY")) {
+					myOutLine.setGenSysNumber(lineObj.getToken(1));
+					myOutLine.setGenPrintClass(lineObj.getToken(3));
+					myOutLine.printOutLine();
 					somethingToPrint = true;
-				}
-				else if (aLine.contains(" DEST=")) {
-					genPrintDest = lineObj.getToken(2);
-				}
-				else if (aLine.contains(" OUTPUT=")) {
-					genFormId = lineObj.getToken(2);
-					writeOutput(jobName, jobProgName, jobClass, jobMsgClass, jobMemberName, jobEnvironment, jobRegion,
-							jobProgram, genSysNumber, genPrintClass, genWriterName, genPrintDest, genFormId);
-					genSysNumber = "";
-					genPrintClass = "";
-					genWriterName = "";
-					genPrintDest = "";
-					genFormId = "";
+				} else if (aLine.contains(" DEST=")) {
+					myOutLine.setGenPrintDest(lineObj.getToken(2));
+				} else if (aLine.contains(" OUTPUT=")) {
+					myOutLine.setGenFormId(lineObj.getToken(2));
+					myOutLine.printOutLine();
+					myOutLine.clearPrintParms();
 					somethingToPrint = false;
 				}
 			}
 			if (somethingToPrint) {
-				writeOutput(jobName, jobProgName, jobClass, jobMsgClass, jobMemberName, jobEnvironment, jobRegion,
-						jobProgram, genSysNumber, genPrintClass, genWriterName, genPrintDest, genFormId);
+				myOutLine.printOutLine();
 			}
-
 		} catch (Exception x) {
 			// TODO
 		} finally {
@@ -134,13 +85,5 @@ public class ParseInputFile {
 					e.printStackTrace();
 				}
 		}
-	}
-
-	private static void writeOutput(String jobName, String jobProgName, String jobClass, String jobMsgClass,
-			String jobMemberName, String jobEnvironment, String jobRegion, String jobProgram, String genSysNumber,
-			String genPrintClass, String genWriterName, String genPrintDest, String genFormId) {
-		System.out.println(jobName + ',' + jobProgName + ',' + jobClass + ',' + jobMsgClass + ',' + jobMemberName + ','
-				+ jobEnvironment + ',' + jobRegion + ',' + jobProgram + ',' + genSysNumber + ',' + genPrintClass + ','
-				+ genWriterName + ',' + genPrintDest + ',' + genFormId);
 	}
 }
